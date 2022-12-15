fbda859e67efbbc07598a0a1f35bb77fd63af350
MarwanMagdy72 <115204176+MarwanMagdy72@users.noreply.github.com>
2022-12-15 04:07:47 +0200

    Create Simple Programs Using C# Lang

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace New_folder
+
+{
+    class program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Enter Size of numbers : ");
+            var max = 0 ;
+            var Size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter The numbers : ");
+            for(var i =1; i<=Size;i++){
+                
+                var num = Convert.ToInt32(Console.ReadLine());
+
+                if(num>max){
+
+                    max=num;
+                }
+                
+            }
+            Console.WriteLine("The Max Number is : " + max);
+        }
+    }
+}
