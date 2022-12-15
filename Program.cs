1ad4ef079ea7ce70e1563a52d9d9da855eb48143
MarwanMagdy72 <115204176+MarwanMagdy72@users.noreply.github.com>
2022-12-15 04:36:14 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,93 @@
 ﻿using System;
-namespace New_folder
+namespace C__Tasks;
+public abstract class Person{
+    public string Name;
+    public int Age;
+    
+    public Person(string name, int age){
+        Name = name;
+        Age = age;
+    }
+        public abstract void Print();
+}
+public class Student : Person{
+    public int Year;
+    public float Gpa;
+    public Student(string name, int age, int year, float gpa) : base(name, age) 
+    { 
+        Year = year;
+        Gpa = gpa;
+    }
+    public override void Print() {
+        Console.WriteLine
+        ($"Name: {Name}, Age: {Age}, Gpa: {Gpa}");
+    }
+}
 
-{
-    class program
-    {
-        private static void Main()
-        {
-            Console.WriteLine("Enter Size of numbers : ");
-            var max = 0 ;
-            var Size = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter The numbers : ");
-            for(var i =1; i<=Size;i++){
-                
-                var num = Convert.ToInt32(Console.ReadLine());
+public class Staff : Person{
+    public double Salary;
+    public int JoinYear;
+    public Staff(string name, int age, double salary, int joinyear) : base(name, age) 
+    { 
+        Salary = salary;
+        JoinYear = joinyear;
+    }
+    public override void Print() {
+        Console.WriteLine
+        ($"Name: {Name}, Age: {Age}, Salary: {Salary}");
+    }
+    
 
-                if(num>max){
+}
+public class Database{
+    private int _currentIndex;
+    public Person[] People = new Person[50];
+    public void AddStudent(Student student) {
+        People[_currentIndex++] = student;
+    }
+    public void AddStaff(Staff staff) {
+        People[_currentIndex++] = staff;
+    }
+    public void PrintALL(){
+        foreach (var item in People){
+            item?.Print();
+        }
+    }
+}
 
-                    max=num;
-                }
-                
+public class Program{
+    private static void Main(){
+        var database = new Database();
+        while (true) {
+            Console.Write("Option: ");
+            var option = Convert.ToInt32(Console.ReadLine());
+            if (option == 1){
+                Console.Write("Name: ");
+                var name = Console.ReadLine();
+                Console.Write("Age: ");
+                var age = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Year: ");
+                var year = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Gpa: ");
+                var gpa = Convert.ToInt32(Console.ReadLine());
+                var student = new Student(name, age, year, gpa);
+                database.AddStudent(student);
             }
-            Console.WriteLine("The Max Number is : " + max);
+            else if (option ==2){
+                Console.Write("Name: ");
+                var name = Console.ReadLine();
+                Console.Write("Age: ");
+                var age = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Slary: ");
+                var salary = Convert.ToInt32(Console.ReadLine());
+                Console.Write("JoinYear: ");
+                var joinyear = Convert.ToInt32(Console.ReadLine());
+                var staff = new Staff(name, age, salary, joinyear);
+                database.AddStaff(staff);
+            }
+            else if (option == 3){
+                database.PrintALL();
+            }
         }
     }
 }
